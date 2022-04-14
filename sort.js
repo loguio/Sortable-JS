532e12e1e7e04923b918d6aac25855d94bea9fe1
Nathan BOURRY <nathan.bourry@ynov.com>
2022-04-14 11:10:32 +0200

    Le trie des super héros en fonction de l'id

--- sort.js
+++ sort.js
@@ -1,3 +1,6 @@
+let data = await fetch("https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json")
+data = await data.json()
+    
 function sort_Alpha_Num (arrayObjet) {
     function compare(firstValue, secondValue) {
         if (firstValue.value < secondValue.value)
@@ -9,3 +12,13 @@ function sort_Alpha_Num (arrayObjet) {
     return arrayObjet.sort(compare)
 }
 
+function TrieObjetID (arrayObjet = sort_Alpha_Num(arrayObjet)) {
+    let array = []
+    for (let index = 0; index < arrayObjet.length; index++) {
+        const element = arrayObjet[index];
+        array.push(element.id)
+    }
+    return data.sort(function(a, b){  
+        return array.indexOf(a.id) - array.indexOf(b.id);
+    })
+}
