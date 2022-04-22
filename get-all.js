aff6013e338f7182eca8523215b7dbffafe9260c
Yassine Ennouhi <root@LAPTOP-8BN54FDS.localdomain>
2022-04-22 23:20:28 +0200

    navigate through pages (last, first, previous, next)

--- get-all.js
+++ get-all.js
@@ -1,6 +1,11 @@
  var data = {}
  let numberOfItems = 20;
  let firstIndex = 0;
+ let currentPage = 1;
+ let max = 532;
+ let maxPages = Math.ceil(max / numberOfItems);
+
+
  async function getAll(obj) {
      var hero = await fetch('https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json');
      var heroData = await hero.json()
@@ -10,10 +15,14 @@
 
  let data2 = getAll(data)
 
- data2.then(function first(result) {
-     showData(result, numberOfItems)
- })
+ function showPage() {
+     data2.then(function first(result) {
+             showData(result, numberOfItems)
+         })
+         //  showPageInfo();
+ }
 
+ showPage();
 
  function showData(data, numberOfItems) {
      for (let i = firstIndex; i < firstIndex + numberOfItems; i++) {
@@ -21,16 +30,51 @@
      }
  }
 
- //  function nextPage() {
- //      if firstIndex + numberOfItems <=
- //          firstIndex += numberOfItems;
- //      showData(data, numberOfItems)
- //  }
+ function nextPage() {
+     var table = document.getElementById("tableData");
+     table.innerHTML = ""
+     if (firstIndex + numberOfItems <= max) {
+         firstIndex += numberOfItems;
+         currentPage++;
+         showPage();
+     }
+ }
 
+ function previous() {
+     var table = document.getElementById("tableData");
+     table.innerHTML = ""
+     if (firstIndex - numberOfItems >= 0) {
+         firstIndex -= numberOfItems
+         currentPage--;
+         showPage();
+     }
+ }
+
+ function firstPage() {
+     var table = document.getElementById("tableData");
+     table.innerHTML = ""
+     firstIndex = 0
+     currentPage = 1;
+     showPage();
+ }
+ console.log(currentPage)
+ console.log(maxPages)
+
+ function lastPage() {
+     var table = document.getElementById("tableData");
+     table.innerHTML = ""
+     firstIndex = (maxPages * numberOfItems) - numberOfItems;
+     currentPage = maxPages;
+     showPage();
+ }
+
+ //  function showPageInfo() {
+ //      document.getElementById('pageNo').innerHTML = ` Page ${currentPage} / ${maxPages} `
+ //  }
 
  function addRow(top, heroes) {
      // GET TABLE
-     var table = document.getElementById("table");
+     var table = document.getElementById("tableData");
 
      // INSERT ROW
      if (top) { var row = table.insertRow(); } else { var row = table.insertRow(); }
